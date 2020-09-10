38717439fcaed62365a0830c884e76ff956457e1
Succulence <48991584+Succulence@users.noreply.github.com>
2020-09-10 12:40:04 +0200

    Satt opp databasefilen og lagt til en "legg til kunde test funksjon". Noen nye mapper

---
+++ oblig1-1/Controllers/BussController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using oblig1_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace oblig1_1.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class BussController : ControllerBase
+    {
+        private readonly BestillingDB _db;
+
+        public BussController(BestillingDB db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> LagreKunde(Kunde kunde)
+        {
+            try
+            {
+                _db.Kunder.Add(kunde);
+                await _db.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ oblig1-1/wwwroot/js/forside.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    console.log("Test");
+});
+
+function lagreKunde() {
+
+    const kunde = {
+        navn: $('#fra').val(),
+        telefon: $("#til").val()
+    }
+
+
+    const url = "Buss/LagreKunde";
+    $.post(url, kunde, function (OK) {
+        console.log(kunde.navn);
+        if (OK) {
+            window.location.href = 'forside.html';
+        } else {
+            $("#feil").html("Feil i db - bestilling");
+        }
+    });
+
+}
+
