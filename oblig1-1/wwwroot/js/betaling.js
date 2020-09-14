971bac562c1e321ced502f43a95a4288bb30797c
theajanvin <theajanvin@gmail.com>
2020-09-14 11:19:18 +0200

    Fiksa drop-down meny slik at man kun ser relevant info etter valgt betalingsmåte

--- oblig1-1/wwwroot/js/betaling.js
+++ oblig1-1/wwwroot/js/betaling.js
@@ -1,1 +1,18 @@
 ﻿
+// funksjon som gjør at vi kun viser input-bokser som er relevant til valgt 
+// betalingsmåte 
+function valgtBetaling() {
+    var valgt = $('#betaling').val();
+    if (valgt == 'kort') {
+        $('#kort').show();
+        $('#vipps').hide();
+    }
+    else if (valgt == 'vipps') {
+        $('#vipps').show();
+        $('#kort').hide();
+    }
+    else {
+        $('#vipps').hide();
+        $('#kort').hide();
+    }
+}
