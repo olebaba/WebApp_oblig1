9cff77491307bf28a4e0f8c759ec030f2361816b
Zandra Max <s333772@oslomet.no>
2020-10-23 12:57:59 +0200

    lagre ny ruteavgang fungerer np

--- oblig1-1/wwwroot/js/lagreRuteAvgang.js
+++ oblig1-1/wwwroot/js/lagreRuteAvgang.js
@@ -1,18 +1,24 @@
-﻿function lagreRuteAvgang() {
-    const ruteavgang = {
-        dato: $("#dato").val(),
-
+﻿function validerOgLagreRuteAvgang() {
+    const datoOK = validerDato($("#dato").val());
+    const ruteOK = validerRute($("#rute").val())
+    if (datoOK && ruteOK) {
+        lagreRuteAvgang();
     }
-    const url = "Bestilling/LagreRuteAvgang";
-    $.post(url, ruteavgang, function () {
+}
+function lagreRuteAvgang() {
+    var rutenavn = $("#rute").val();
+    var dato = $("#dato").val();
+    var innparameter = { argumenter: [rutenavn, dato] };
+    const url = "Bestilling/NyRuteAvgang";
+    $.post(url, innparameter, function () {
         window.location.href = 'admin.html';
     })
-        .fail(function (feil) {
-            if (feil.status == 401) {
-                window.location.href = 'innlogging.html';
-            }
-            else {
-                $("#feil").html("Feil på server - prøv igjen senere");
-            }
-        });
+    .fail(function (feil) {
+        if (feil.status == 401) {
+            window.location.href = 'innlogging.html';
+        }
+        else {
+            $("#feil").html("Feil på server - prøv igjen senere");
+        }
+    });
 }
